36d61a8d0f5d658701c51bbdec5e97fd49f430d2
DevKyereboah <sarah.owusu@amalitech.com>
2024-02-23 12:46:28 +0000

    git commit -m "selenium java concept-Data types"

---
+++ src/main/java/introduction/HelloWorld.java
@@ -0,0 +1,18 @@
+package introduction;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        //This is my first java program
+        /* this is my second comment
+        *
+        *
+        * */
+
+        String word = "hello word";
+
+        System.out.println(word);
+
+
+    }
+}
